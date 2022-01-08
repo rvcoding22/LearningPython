4cf1eb47c59adbe03975ee0754f690960e780892
rvcoding22 <97313974+rvcoding22@users.noreply.github.com>
2022-01-08 19:14:42 +0000

    Solved Fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,2 +1,7 @@
-def fibonacci():
-  print('Fibonacci function')
+def fibonacci(num):
+  if num == 1:
+    return 1
+  elif num == 2:
+    return 2
+  else:
+    return fibonacci(num-1) + fibonacci(num-2)
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from fibonacci import *
 if __name__=="__main__":
   mult3and5()
   print('\n')
-  fibonacci()
+  # 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+  print(fibonacci(40))
   
